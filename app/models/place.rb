8322f3da315733e56e08186490f0603177512a5a
ChanneDesign <channebertrand@gmail.com>
2016-07-10 15:52:18 -0400

    add geocoder

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ActiveRecord::Base
-	# geocoded_by :address
-	# after_validation :geocode, if: ->(obj){ obj.address.present? && obj.address_changed? }
+	geocoded_by :address
+	after_validation :geocode, if: ->(obj){ obj.address.present? && obj.address_changed? }
 
 	scope :by_created_at, -> { order('created_at DESC') }
 end
