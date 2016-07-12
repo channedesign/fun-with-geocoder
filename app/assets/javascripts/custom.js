c177cd0dbe4ea1447cd0d4ee1e0d4c6b7dc4ef5c
ChanneDesign <channebertrand@gmail.com>
2016-07-12 09:56:19 -0400

    add makers to map

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,6 @@
 jQuery(function() {
 	var lat_field, lng_field;
+	var markersArray = [];
 	lat_field = $('#place_latitude');
 	lng_field = $("#place_longitude");
 	window.initMap = function() {
@@ -12,16 +13,28 @@ jQuery(function() {
       zoom: 8
     });
     map.addListener('click', function(e) {
-    	console.log(e.latLng)
+    	placeMarkerToMap(e.latLng, map)
     	updateFields(e.latLng)
-
     }); 
 	}
 
 	function updateFields(latLng) {
 		lat_field.val(latLng.lat());
 		lng_field.val(latLng.lng());
-		
+	}
+
+	function placeMarkerToMap(latLng, map) {
+		var marker;
+		while (markersArray.length) {
+			console.log('yo')
+			markersArray.pop().setMap(null);
+		}
+		marker = new google.maps.Marker({
+	    map: map,
+	    position: latLng
+	  });
+	  map.panTo(latLng);
+	  markersArray.push(marker);
 	}
 
 });
